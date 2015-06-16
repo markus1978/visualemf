dac5e2602603e14c7b470bc58c498d628909e3d6
Markus Scheidgen <markus.scheidgen@gmail.com>
2015-06-16 23:06:09 +0200

    Added an example gwt based D3 visualization.

---
+++ plugins/de.hub.visualemf.gwt.examples/src/de/hub/visualemf/gwt/examples/client/GwtExamples.java
@@ -0,0 +1,13 @@
+package de.hub.visualemf.gwt.examples.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class GwtExamples implements EntryPoint {
+
+	public void onModuleLoad() {
+		ChordDiagram chordDiagram = new ChordDiagram();
+		RootPanel.get().add(chordDiagram);
+		chordDiagram.render();
+	}
+}
